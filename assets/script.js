475280f444d535d3384ce94e214df7fb50d1a00e
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-05-08 19:25:13 -0400

    fixed footer position

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,65 @@
-//base urls
-var bandInfoBaseURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=";
-var lyricsBaseURL = "https://api.lyrics.ovh/v1/"
+//base urls
+var bandInfoBaseURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=";
+var lyricsBaseURL = "https://api.lyrics.ovh/v1/"
+$("#searchBtn").on("click", function(){
+    // clear out the contents of the current container
+    $("#artistimg").empty();
+    $("#artistThumb").empty();
+    $("#link").empty();
+    $("#genre").empty();
+    $("#userSearch").empty();
+    $("#userSearch2").empty();
+    $("#lyrics").empty()
+    
+    // grab the values of the text boxes and apply them to variables
+    var artistSearch = $("#userSearch").val().trim();
+    var fullBandURL = bandInfoBaseURL + artistSearch;
+    var songName = $("#userSearch2").val().trim();
+    var fullSongUrl = lyricsBaseURL + artistSearch + "/" + songName 
+    $.ajax({
+        url: fullBandURL,
+        method: "GET"
+    }).then(function(response) {
+        // directions to logo png --- id from html = artistimg
+        // create the image tag
+        let logoTag = $("<img>")
+        // apply the src attribute and attatch the directions to the logo
+        logoTag.attr("src", response.artists[0].strArtistLogo)
+        logoTag.attr("width", "300px")
+        // append image to the page
+        $("#artistimg").append(logoTag)
 
-$("#searchBtn").on("click", function(){
-    // clear out the contents of the current container
-    $("#artistimg").empty();
-    $("#addInfo").empty();
-    $('#link').empty();
-    
-    // grab the values of the text boxes and apply them to variables
-    var artistSearch = $("#userSearch").val().trim();
-    var fullBandURL = bandInfoBaseURL + artistSearch;
-    var songName = $("#userSearch2").val().trim();
-    var fullSongUrl = lyricsBaseURL + artistSearch + "/" + songName 
+        // directions to genre/style
+        let genrePTag = $("<p>");
+        genrePTag.text("Genre: " + response.artists[0].strStyle)
+        genrePTag.attr("id", "genreButton")
+        $("#genre").append(genrePTag)
 
-    $.ajax({
-        url: fullBandURL,
-        method: "GET"
-    }).then(function(response) {
-        // directions to logo png --- id from html = artistimg
-        // create the image tag
-        let logoTag = $("<img>")
-        // apply the src attribute and attatch the directions to the logo
-        logoTag.attr("src", response.artists[0].strArtistLogo)
-        logoTag.attr("width", "300px")
-        // append image to the page
-        $("#artistimg").append(logoTag)
+        // directions to facebook link
+        // auto skips if band does not have facebook, so no conditional needed 
+        let FBLinkTag = $("<a>");
+        let FBLinkTextTag = $("<p>");
+        FBLinkTextTag.text(response.artists[0].strFacebook)
+        FBLinkTag.append(FBLinkTextTag);
+        FBLinkTag.attr("href", FBLinkTextTag);
+        FBLinkTextTag.attr("id", "linkBox");
+        $("#link").append(FBLinkTag)
 
-        // directions to the artist name
-        var artistPTag = $("<p>")
-        artistPTag.text(response.artists[0].strArtist)
-        $("#artistimg").append(artistPTag)
-
-        // directions to genre/style
-        let genrePTag = $("<p>");
-        genrePTag.text(response.artists[0].strStyle)
-        $("#genre").append(genrePTag)
-
-        // directions to facebook link
-        // auto skips if band does not have facebook, so no conditional needed 
-        let FBLinkTag = $("<a>");
-        let FBLinkTextTag = $("<p>");
-        FBLinkTextTag.text(response.artists[0].strFacebook)
-        FBLinkTag.append(FBLinkTextTag);
-        FBLinkTag.attr("href", FBLinkTextTag);
-        $("#link").append(FBLinkTag)
-
-        // directions to artist banner
-        let artistThumbTag = $("<img>");
-        artistThumbTag.attr("src", response.artists[0].strArtistThumb)
-        artistThumbTag.attr("width", "200px")
-        $("#artistThumb").append(artistThumbTag)
-    })
-
-    $.ajax({
-        url: fullSongUrl,
-        method: "GET"
-    }).then(function(response) {
-        $("#lyrics").text(response.lyrics)
-        // if there are no lyrics from the json string
-        if (response.lyrics === "") {
-            $("#lyrics").text("No lyrics obtained, please try again.");
-        }
-    })
+        // directions to artist banner
+        let artistThumbTag = $("<img>");
+        artistThumbTag.attr("src", response.artists[0].strArtistThumb)
+        artistThumbTag.attr("width", "200px")
+        $("#artistThumb").append(artistThumbTag)
+    })
+    $.ajax({
+        url: fullSongUrl,
+        method: "GET"
+    }).then(function(response) {
+        $("#lyrics").text(response.lyrics)
+        // if there are no lyrics from the json string
+        if (response.lyrics === "") {
+            $("#lyrics").text("No lyrics obtained, please try again.");
+        }
+    })
 })
+
